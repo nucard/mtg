6693448d4c8fd9c2c3806626a616d674d06a7228
ben <ben@jammerware.com>
2018-07-11 00:26:08 -0400

    Added dynamic base url for non-prod scenarios

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export class Config {
-    public baseUrl = 'https://nucard-mtg.herokuapp.com';
+    public baseUrl = process.env.NODE_ENV === 'production' ? 'https://nucard-mtg.herokuapp.com' : 'http://localhost:/5223';
     public port = process.env.PORT || 5223;
 }
